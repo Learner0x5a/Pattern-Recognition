ea866eafb56cf770f1e4fb54c3a19f86e4bb05a7
kid <zhuwy15@gmail.com>
2020-03-27 22:40:44 +0800

    hyperplane

--- HW3/zhuwy/non-linear-classify.py
+++ HW3/zhuwy/non-linear-classify.py
@@ -5,19 +5,10 @@ from sklearn.metrics import mean_squared_error
 import matplotlib
 matplotlib.use('AGG')
 import matplotlib.pyplot as plt
+fig = plt.figure()
 
 W1 = np.array([(1,1),(2,2),(3,3),(1,3),(3,1)])
 W2 = np.array([(1,2),(2,1),(2,3),(3,2)])
-fig = plt.figure()
-plt.scatter(W1[:,0],W1[:,1],marker='x',label='W1',c='b')
-plt.scatter(W2[:,0],W2[:,1],marker='o',label='W2',c='r')
-plt.xlim((0, 4))
-plt.ylim((0, 4))
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend()
-plt.savefig('2d.png')
-plt.clf()
 W = np.vstack((W1,W2))
 x = W[:,0]
 y = W[:,1]
@@ -29,8 +20,8 @@ W = np.concatenate((W,Z),axis=1)
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = Axes3D(fig)
-X = np.linspace(0,4,50)
-Y = np.linspace(0,4,50)
+X = np.linspace(0,4,150)
+Y = np.linspace(0,4,150)
 X, Y = np.meshgrid(X, Y)
 Z = np.full(X.shape,1/2)
 ax.plot_surface(X,Y,Z,color='green',alpha=0.2)
@@ -41,9 +32,33 @@ ax.scatter(W[0:5,0], W[0:5,1], W[0:5,2],c='b',label='W1')
 ax.scatter(W[5:,0], W[5:,1], W[5:,2],c='r',label='W2')
 plt.legend()
 plt.savefig("3d.png")
+plt.clf()
+
+plt.scatter(W1[:,0],W1[:,1],marker='x',label='W1',c='b')
+plt.scatter(W2[:,0],W2[:,1],marker='o',label='W2',c='r')
+plt.xlim((0, 4))
+plt.ylim((0, 4))
+plt.xlabel('x')
+plt.ylabel('y')
+
+half = np.abs(X-2) - np.abs(Y-2)
+X1 = X[np.where(half<=0)]
+Y1 = 2 + np.sqrt(np.power(X1-2,2) + 1/2)
+Y2 = 2 - np.sqrt(np.power(X1-2,2) + 1/2)
+X2 = X[np.where(half<=0)]
+Y3 = 2 + np.sqrt(np.power(X2-2,2) - 1/2)
+Y4 = 2 - np.sqrt(np.power(X2-2,2) - 1/2)
+plt.scatter(X1,Y1,s=1,c='g',label='hyperplane')
+plt.scatter(X1,Y2,s=1,c='g')
+plt.scatter(X2,Y3,s=1,c='g')
+plt.scatter(X2,Y4,s=1,c='g')
+plt.legend()
+plt.savefig('2d.png')
+plt.clf()
+
+
 
 from sklearn.manifold import TSNE
-
 def plot_with_labels(low_dim_embs, labels, filename):   # 绘制词向量图
     plt.figure()  
     for i, label in enumerate(labels):
@@ -69,4 +84,3 @@ tsne = TSNE(n_components=2)
 low_dim_embs = tsne.fit_transform(W)
 labels = ['W1','W1','W1','W1','W1','W2','W2','W2','W2']
 plot_with_labels(low_dim_embs, labels, 'tsne.png')
-
