9aca96b98edbe40bb737c3fb3ed88f7c8ec9b7ef
kid <zhuwy15@gmail.com>
2020-04-03 22:32:35 +0800

    knn

--- HW4/zhuwy/kNN.py
+++ HW4/zhuwy/kNN.py
@@ -1,7 +1,6 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import mean_squared_error
 import numpy as np 
-import numpy as np 
 import matplotlib
 matplotlib.use('AGG')
 import matplotlib.pyplot as plt
@@ -15,12 +14,48 @@ Zy = np.reshape(Zy,(Zy.shape[0]*Zy.shape[1],))
 X_test = np.array(list(zip(Zx,Zy)))
 print(X_test.shape)
 
-plt.scatter(X_test[:,0],X_test[:,1],s=1)
 
-plt.savefig('knn.png')
 
-model = KNeighborsClassifier(n_neighbors=3)
+f = open('trainData.txt','r')
+plt_x1 = []
+plt_y1 = []
+plt_x2 = []
+plt_y2 = []
+label_train = []
+for line in f.readlines():
+    data = line.split()
+    if data[2] == '1':
+        plt_x1.append(float(data[0]))
+        plt_y1.append(float(data[1]))
+    else:
+        plt_x2.append(float(data[0]))
+        plt_y2.append(float(data[1]))
+    label_train.append(float(data[2])-1)
+f.close()
 
-########### ??? 标签是什么
+x_train = plt_x1 + plt_x2
+y_train = plt_y1 + plt_y2
+x_train = np.array(x_train)[:,np.newaxis]
+y_train = np.array(y_train)[:,np.newaxis]
 
-# model.fit(X)  
+X = np.concatenate((x_train,y_train),axis=1)
+label_train = np.array(label_train)
+
+print(X.shape,label_train.shape)
+
+
+for k in range(1,10):
+    model = KNeighborsClassifier(n_neighbors=k)
+    model.fit(X,label_train)
+
+    label_test = model.predict(X_test)
+
+    plt.scatter(X_test[:,0],X_test[:,1],c=label_test)
+    plt.scatter(plt_x1,plt_y1,c='r')
+    plt.scatter(plt_x2,plt_y2,c='g')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('k='+str(k))
+    plt.savefig('images/knn-'+str(k)+'.png')
+    plt.clf()
+
